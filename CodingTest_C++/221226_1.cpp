48243403cfe6c6185eac3f4ed89b84b3400ffe80
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-12-26 10:14:59 +0900

    주석 추가

--- CodingTest_C++/221226_1.cpp
+++ CodingTest_C++/221226_1.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main() {
-	vector<string> sv;
+	vector<string> sv;	// 문자열 vector 생성
 	string name;
 	cout << "이름을 5개 입력해라" << endl;
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++) {		// 한 줄에 한 개씩 5개의 이름을 입력받는다
 		cout << i + 1 << ">>";
 		getline(cin, name);
 		sv.push_back(name);
 	}
-	name = sv.at(0);
+	name = sv.at(0);		// vector의 첫 원소
 	for (int i = 1; i < sv.size(); i++) {
-		if (name < sv[i])
-			name = sv[i];
+		if (name < sv[i])		// sv[i]의 문자열이 name보다 사전에서 뒤에 나옴(max 알고리즘)
+			name = sv[i];		// name을 sv[i]의 문자열로 변경
 	}
 	cout << "사전에서 가장 뒤에 나오는 이름은 " << name << endl;
 }
